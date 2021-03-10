20654f4ee7d583a06db9e16051313aa1a5974056
Akshay Nair <phenax5@gmail.com>
2021-03-10 22:32:20 +0530

    Fixes lsp setup

--- default.nix
+++ default.nix
@@ -1,10 +1,20 @@
 {}:
 with import <nixpkgs> {};
+let
+  ocamlDeps = with ocamlPackages; with nodePackages; [
+    ocaml
+    opam
+    merlin
+    ocp-indent
+    ocaml-language-server
+  ];
+  systemDeps = with pkgs; [
+    nodejs-15_x
+    yarn
+    docker-compose
+  ];
+in
 stdenv.mkDerivation {
   name = "bs-optics";
-  buildInputs = with pkgs; [
-    docker-compose
-    ocamlPackages.merlin
-    ocamlPackages.ocaml-lsp
-  ];
+  buildInputs = systemDeps ++ ocamlDeps;
 }
